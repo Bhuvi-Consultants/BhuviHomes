22adc6c7880932d6ca9ca1dd44904be00af3b6f4
SShubham kumar <shubhamkrg1819@gmail.com>
2026-04-29 16:19:26 +0530

    fix images.domains deprecated issue while build

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@
 const nextConfig = {
   /* config options here */
   images: {
-    domains: ["images.unsplash.com", "lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mages.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
   },
 };
 
