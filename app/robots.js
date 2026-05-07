2d687fa6c74136cb991abce4cfbf6616356eff9a
SShubham kumar <shubhamkrg1819@gmail.com>
2026-05-07 17:11:00 +0530

    fix robots.txt production & staging generation

---
+++ app/robots.js
@@ -0,0 +1,13 @@
+const isProduction =
+  process.env.NEXT_PUBLIC_SITE_URL === "https://bhuvihomes.in";
+
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: isProduction ? "/" : "",
+      disallow: isProduction ? "" : "/",
+    },
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
+  };
+}
